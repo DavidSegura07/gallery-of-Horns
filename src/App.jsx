7693ca6715ec53c71e4af9289034adfbc34aa7e4
David Segura <dsegura07@yahoo.com>
2023-09-14 21:54:36 -0700

    come on

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@
 `Header`, `Gallery`, and `Footer` from their respective files. */
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Header.jsx';
-import Gallery from './Gallery.jsx';
+import Header from '../components/Header.jsx';
+import Gallery from '../components/Gallery.jsx';
 import Footer from './components/Footer.jsx';
 
 function App() {
