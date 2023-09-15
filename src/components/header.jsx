838cb06b5d44f301010a8b840ca12347af40a446
David Segura <dsegura07@yahoo.com>
2023-09-14 21:46:54 -0700

    added a small summary of the site.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,6 +9,7 @@ class Header extends React.Component {
             <header>
                 <h1>Gallery of Horns
                 </h1>
+                <p>Welcome to the Gallery of Horns, a one-of-a-kind digital exhibit that showcases a curated collection of magnificent horned beasts. Each click leads you deeper into a realm where the creatures grow increasingly captivating and intimidating. Starting with the innocent UniWhal, every successive image reveals a beast more formidable and awe-inspiring than the last. As you navigate through the gallery, you'll have the opportunity to mark your favorite creatures. Dare to venture through our gallery, but be warned: the beasts get scarier by the picture. An interactive experience that's equal parts beauty and adrenaline, the Gallery of Horns is not for the faint of heart.</p>
             </header>    
         )
     }
